d245e21e10539edd25b16a38247cc4a6ef92d980
dell <dell@DELL-PC>
2019-10-16 01:33:26 +0530

    including the application for demo

---
+++ Console_AssemblyConcept/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using demoDLL;
+
+namespace Console_AssemblyConcept
+{
+    public class Program
+    {
+
+        public void newMethod()
+        {
+            Class1 obj = new Class1();
+        }
+
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine(obj.demoMethod());
+            //Console.WriteLine(obj);
+            SubClass objSubClass = new SubClass();
+            objSubClass.Test();
+            //objSubClass;
+            BaseClass obj = new BaseClass();
+            obj.Test();
+            Console.WriteLine("This is a msg from console application");
+            Console.ReadLine();
+        }
+
+        public class BaseClass
+        {
+            protected internal virtual void Test()
+            {
+                Console.WriteLine("protected Test method inside base class");
+            }
+
+            protected internal void Test1()
+            {
+                Console.WriteLine("protected Test method inside base class");
+            }
+        }
+
+        public class SubClass : BaseClass
+        {
+            public new void Test()
+            {
+                base.Test();
+            }
+        }
+
+    }
+
+    
+}
---
+++ demoDLL/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoDLL
+{
+    public class Class1
+    {
+        public string demoMethod()
+        {
+            return "This is a msg from dll";
+        }
+
+        //internal string myProp = "Is it Visible?";
+        internal protected string checkInternal()
+        {
+            return "This is internal";
+        }
+    }
+}
